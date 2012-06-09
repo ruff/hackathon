b437de5b41fc7ea44664b0d06da464d5eedde810
Stefan Negrea <snegrea@redhat.com>
2012-06-09 15:20:21 -0500

    Using JSON pickler...
    use this command to install it:
    sudo pip install -U jsonpickle

--- backend/transscrapper/structure.py
+++ backend/transscrapper/structure.py
@@ -1,3 +1,15 @@
-class Employee:
+class Response:
    'Common base class for all employees'
-   empCount = 0
+   def __init__(self):
+       self.saddr = 'Start address'
+       self.daddr = 'End address'
+       self.mpg = 16
+
+    #fuel_cost: 1756 // cents
+    #landmarks: ["DMA", "Nasher", "etc"]
+    #parking_price: 5
+    #drive_time: 50 // minutes
+    #transit_time: 60 // minutes
+    #weather_condition: "101 F 20% chance of rain"
+    #drive_co2: 5 // tons
+    #transit_price: 400 // cents = 0
