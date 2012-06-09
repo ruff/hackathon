ea0084d3ee93e7d76d76a3829dbccf33cb53843c
Alex Soncodi <asoncodi@gmail.com>
2012-06-09 17:40:12 -0500

    Add donate button and test mode.

--- backend/transscraper/static/js/transscraper.js
+++ backend/transscraper/static/js/transscraper.js
@@ -1,29 +1,50 @@
 
 $(function() {
+
+	var testMode = true;
 	
-	$('#clear').click(function() {
+	$('#clear').click(function(e) {
+		e.preventDefault();
 		$('input[type="text"]').val('');
+		$('#results').fadeOut('fast');
 	});
 	
-	$('form').submit(function(e) {
+	$('#searchform').submit(function(e) {
 		e.preventDefault();
 		
-		var request = {
-			'car_model': $('#carmodel').val(),
-			'start_address': $('#saddr').val(),
-			'car_make': $('#carmake').val(),
-			'destination_address': $('#daddr').val(),
-			'number_people': $('#numpeople').val()
-		};
-		
-		$.get(
-			'/api',
-			request,
-			function(response) {
-				renderResponse(response);
-			},
-			'json'
-		);
+		if (testMode) {
+			var response = {
+				'mpg': 16,
+				'fuel_cost': 1756,
+				'transit_time': 60,
+				'landmarks': ['DMA', 'Nasher'],
+				'start_address': '2900 West Plano Parkway Plano, Texas 75075',
+				'drive_time': 50,
+				'destination_address': '2900 West Plano Parkway Plano, Texas 75075',
+				'parking_price': 5,
+				'drive_co2': 5,
+				'transit_price': 400,
+				'weather_condition': '101 F, 20% chance of rain'
+			};
+			
+			renderResponse(response);
+		}
+		else {
+			$.get(
+				'/api',
+				{
+					'car_model': $('#carmodel').val(),
+					'start_address': $('#saddr').val(),
+					'car_make': $('#carmake').val(),
+					'destination_address': $('#daddr').val(),
+					'number_people': $('#numpeople').val()
+				},
+				function(response) {
+					renderResponse(response);
+				},
+				'json'
+			);
+		}
 		
 		function renderResponse(response) {
 			$('#mpg').html(response.mpg);
@@ -37,6 +58,8 @@ $(function() {
 			$('#drive-co2').html(response.drive_co2);
 			$('#transit-price').html(response.transit_price);
 			$('#weather-condition').html(response.weather_condition);
+			
+			$('#results').hide().fadeIn('fast');
 		}
 	});
 });
