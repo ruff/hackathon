d44f8eb2f156c028f9d3cb2b356d18f9ef6875ca
Stefan Negrea <snegrea@redhat.com>
2012-06-09 15:49:30 -0500

    Update the test objects with some realistic data.

--- backend/transscrapper/structure.py
+++ backend/transscrapper/structure.py
@@ -1,15 +1,15 @@
 class Response:
    'Response'
    def __init__(self):
-       self.saddr = 'Start address'
-       self.daddr = 'End address'
+       self.saddr = '2900 West Plano Parkway Plano, Texas 75075'
+       self.daddr = '2900 West Plano Parkway Plano, Texas 75075'
        self.mpg = 16
        self.fuel_cost = 1756
-       self.landmarks =["DMA", "Nasher", "etc"]
+       self.landmarks =["DMA", "Nasher"]
        self.parking_price = 5
        self.drive_time = 50
        self.transit_time = 60
-       self.weather_condition = '101 F 20% chance of rain'
+       self.weather_condition = '101 F, 20% chance of rain'
        self.drive_co2 = 5
        self.transit_price = 400
 
@@ -17,8 +17,8 @@ class Response:
 class Resquest :
     'Request'
     def __init__(self):
-        self.saddr = "Start Address"
-        self.daddr = "End Address"
+        self.saddr = "2900 West Plano Parkway Plano, Texas 75075"
+        self.daddr = "2900 West Plano Parkway Plano, Texas 75075"
         self.numpeople = 3
         self.carmake = "WMB"
         self.carmodel = "5X"
