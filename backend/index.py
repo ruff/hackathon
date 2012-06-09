3b0a1ea769ec70132376bb213ed16f038ad9be6f
Matt Havener <info@steepturnsoftware.com>
2012-06-09 15:22:02 -0500

    some scraping

--- backend/index.py
+++ backend/index.py
@@ -1,7 +1,24 @@
 from flask import Flask
 import json
+import demjson
+import httplib
 
 app = Flask(__name__)
+
+def pull_maps(saddr, daddr):
+    conn = httplib.HTTPConnection("maps.google.com")
+    conn.request("GET", "/maps?saddr=%s&daddr=%s&dirflg=r&ttype=now&noexp=0&noal=0&sort=def&mra=ltm&t=m&z=13&start=0&output=json")
+    r = conn.getresponse().read()
+    def removeNonAscii(s): return "".join(filter(lambda x: ord(x)<128, s))
+    r_fixed = removeNonAscii(r[9:]) # remove TM chars, remove front while(1);
+    js = demjson.decode(r_fixed)
+
+
+
+
+    
+
+
 
 @app.route("/", methods=['GET'])
 def hello():
