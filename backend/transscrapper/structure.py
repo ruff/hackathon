465efb93c86057b43e220442ee3cd92defc689e6
Stefan Negrea <snegrea@redhat.com>
2012-06-09 15:45:07 -0500

    Create request class.

--- backend/transscrapper/structure.py
+++ backend/transscrapper/structure.py
@@ -1,5 +1,5 @@
 class Response:
-   'Common base class for all employees'
+   'Response'
    def __init__(self):
        self.saddr = 'Start address'
        self.daddr = 'End address'
@@ -12,3 +12,13 @@ class Response:
        self.weather_condition = '101 F 20% chance of rain'
        self.drive_co2 = 5
        self.transit_price = 400
+
+
+class Resquest :
+    'Request'
+    def __init__(self):
+        self.saddr = "Start Address"
+        self.daddr = "End Address"
+        self.numpeople = 3
+        self.carmake = "WMB"
+        self.carmodel = "5X"
