3126f93f0678d02459a9d6c783bb1090bb0bd0da
Stefan Negrea <snegrea@redhat.com>
2012-06-09 19:15:40 -0500

    Finish the API....

--- backend/transscraper/__init__.py
+++ backend/transscraper/__init__.py
@@ -9,6 +9,7 @@ from transscraper.map_api import pull_car_miles_and_time,pull_bus_transit_time
 from transscraper.gas_api import pull_gas_price
 from transscraper.weather_api import weather_as_string
 from transscraper.parking_api import pull_parking_cost
+from transscraper.mpg_api import find_mpg
 
 
 app = Flask(__name__)
@@ -34,7 +35,7 @@ def api():
     response.start_address = parsed_request.start_address
     response.destination_address = parsed_request.destination_address
 
-    response.fuel_cost = pull_gas_price(parsed_request)
+    price_per_gallon = pull_gas_price(parsed_request)
     response.transit_time = pull_bus_transit_time(parsed_request)
     response.weather_condition = weather_as_string(parsed_request)
     (car_miles, response.drive_time) = pull_car_miles_and_time(parsed_request)
@@ -42,9 +43,16 @@ def api():
     
     mpg = find_mpg(parsed_request)
     
+    gallons = float(car_miles) / float(mpg)
+    
+    car_miles = car_miles * 2
+    
+    response.gallons = gallons
     response.mpg = mpg
-    response.landmarks =["DMA", "Nasher"]
-    response.drive_co2 = 5
+    response.fuel_cost = gallons * price_per_gallon / 100
+    response.drive_co2 = gallons * 19  
+    
+    response.landmarks =["Museum", "Big Central Park"]
     response.transit_price = 4
 
     return jsonpickle.encode(response)
--- backend/transscraper/mpg_api.py
+++ backend/transscraper/mpg_api.py
@@ -2,14 +2,16 @@ import csv
 
 
 def find_mpg(request):
-    return find_mpg(request.car_make,request.car_model)
-
-def find_mpg(car_make, car_model):
-    reader = csv.reader(open('static/data/mpg.csv'),delimiter=',')
+    reader = csv.reader(open('transscraper/static/data/mpg.csv'),delimiter=',')
+    car_data_found = False
     for row in reader:
-        if (row[1].count(car_make) != 0):
-            if (row[3].count(car_model) != 0):
+        if (row[1].count(request.car_make) != 0):
+            if (row[3].count(request.car_model) != 0):
                 car_data = row
+                car_data_found = True
                 break
     
-    return car_data[11]
+    if ( car_data_found == True ) :
+        return car_data[11]
+    else :
+        return 21
