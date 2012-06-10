2a07edf5ab7ba908d88ec4d07ff4e8636b2289a7
Stefan Negrea <snegrea@redhat.com>
2012-06-09 19:46:30 -0500

    updates

--- backend/transscraper/__init__.py
+++ backend/transscraper/__init__.py
@@ -58,7 +58,7 @@ def api():
     response.drive_time = float(response.drive_time) / float(60)
     response.transit_time = float(response.transit_time) / float(60)
     
-    response.savings = float(response.fuel_cost) + float(response.parking_price) - float(parsed_request.number_people) * float(response.transit_price);
+    response.savings = float(response.fuel_cost) + float(response.parking_price) - float(parsed_request.number_people) * float(response.transit_price);    
     
     return jsonpickle.encode(response)
 
